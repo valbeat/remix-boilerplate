0d9c21831bba7d2ff6faec0c32f2de14af25b18a
Takuma Kajikawa <kj1ktk@gmail.com>
2024-05-10 17:51:00 +0900

    refactor: remove unused code

--- remix.init/index.js
+++ remix.init/index.js
@@ -2,9 +2,7 @@ const fs = require("node:fs/promises");
 const path = require("node:path");
 
 const main = async ({ rootDirectory }) => {
-	await Promise.all([
-		fs.rm(path.join(rootDirectory, ".github"), { recursive: true }),
-	]);
+	await fs.rm(path.join(rootDirectory, ".github"), { recursive: true });
 };
 
 module.exports = main;
